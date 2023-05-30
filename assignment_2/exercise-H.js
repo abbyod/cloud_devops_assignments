ac4cf47939509e39624b8313c5cf7c8012c12cac
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-05-30 13:00:06 +0200

    exercise H

---
+++ assignment_2/exercise-H.js
@@ -0,0 +1,20 @@
+//Update the file exercise-H.js script in the folder week-1/InClass
+//Design and create a function that:
+    //takes in more than one input
+    //uses string concatenation
+    //this means adding two strings together
+    //performs some form of operation on a number
+    //uses return to return a string
+//Add a comment above your function to explain what it does
+//Call your function and run your script
+//What's the difference between a return and console.log?
+//When would you choose to use functions over the way we have been scripting so far?
+
+//my function will multiply two numbers together
+function multiply(num1, num2) {
+    return (num1 * num2);
+  }
+const result = (multiply);
+
+console.log(result);
+
