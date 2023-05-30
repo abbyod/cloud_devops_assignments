f5ef095a5ab4fd2c5eecb55e31f0d4ddf6710bd9
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-05-30 10:16:47 +0200

    i have finished exercise F

---
+++ assignment_2/exercise-F.js
@@ -0,0 +1,10 @@
+//Update the file exercise-F.js script in the folder week-1/InClass
+//Create two variables numberOfStudents and numberOfMentors
+//Log a message that displays the total number of students and mentors
+
+const numberOfStudents = 48;
+const numberOfMentors = 12;
+const sumTotal = 48 + 12;
+
+console.log("There are " + numberOfStudents + " students and " + numberOfMentors + 
+" mentors. In total, there are " + sumTotal);
