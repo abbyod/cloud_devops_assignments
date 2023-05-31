8492002237c44b4ea88a7bd991776a52f0681540
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-05-31 15:14:03 +0200

    exerciseH

--- assignments_2/exercise-H.js
+++ assignments_2/exercise-H.js
@@ -1,3 +1,5 @@
+//Calculate the exponential of the even numbers from 5 to 20 Using a for loop
+// and the helper functions provided.
 function exponential(number) {
   return number * number;
 }
@@ -5,3 +7,14 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+function calculate() {
+  for (let number =5; number <= 20; number++){
+    if (isEven(number)) {
+      const result = exponential(number);
+      console.log("Exponential of " + number + " is " + result);
+    }
+  }
+}
+
+calculate();
