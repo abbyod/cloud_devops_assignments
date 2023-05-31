3cdc95bb9159c75ace5841b59dfd4dd00e6a4525
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-05-31 15:42:46 +0200

    exercise i

---
+++ assignments_2/exercise-I.js
@@ -0,0 +1,11 @@
+const fruits = ['banana', 'apple', 'strawberry', 'kiwi', 'fig', 'orange'];
+//Now, using the correct indexes, get the following values from the array:
+//strawberry = fruits[2]
+//kiwi = fruits[3]
+//orange = fruits[5]
+//banana = fruits[0]
+//Then, replace 'apple' with 'raspberry', and replace 'fig' with 'pineapple'.
+fruits[1] = 'raspberry'
+'raspberry'
+fruits[4]= 'pineapple'
+'pineapple'
