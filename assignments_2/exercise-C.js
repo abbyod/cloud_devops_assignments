1adcf6d4345866a4f69d340b0a5809adc7ec26c0
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-05-31 12:43:50 +0200

    exerciseC

--- assignments_2/exercise-C.js
+++ assignments_2/exercise-C.js
@@ -9,3 +9,9 @@ function numberChecker(num) {
     return `${num} isn't even a number :(`;
   }
 }
+
+//this function checks numbers to see if they are greater than 20
+//if the number entered is greater than 20, it will return a message saying so 
+//if it is equal to 20, it will it will return a message saying so 
+//if it is less tham 20, it will return a message saying so 
+//if a number is not entered, it will return a message saying the value entered is not a number
